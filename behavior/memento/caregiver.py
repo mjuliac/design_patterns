5ece10e7897e550d49a45a4c20c09c2c69eec7f1
Marc Julià Canal <mcanal76@gmail.com>
2025-11-21 12:36:00 +0100

    refactor: Update Caregiver to manage Originator's state with backup, revert, and history methods.

--- behavior/memento/caregiver.py
+++ behavior/memento/caregiver.py
@@ -1,9 +1,24 @@
 class Caregiver:
-    def __init__(self):
+    def __init__(self, originator):
+        self._originator = originator
         self._mementos = []
         
-    def add_memento(self, memento):
-        self._mementos.append(memento)
+    def backup(self):
+        print("Caretaker: Saving Originator's state...")
+        self._mementos.append(self._originator.create_memento())
         
-    def get_memento(self, index):
-        return self._mementos[index]
+    def revert(self):
+        if not self._mementos:
+            return
+        
+        memento = self._mementos.pop()
+        print(f"Caretaker: Restoring state to: {memento.get_state()}")
+        try:
+            self._originator.set_memento(memento)
+        except Exception as e:
+            self.undo()
+            
+    def show_history(self):
+        print("Caretaker: Here's the list of mementos:")
+        for memento in self._mementos:
+            print(memento.get_state())
